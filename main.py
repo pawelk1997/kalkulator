92bd54a8cf9bcf93361995761997c2ad996f51d5
Paweł Kępczyński <pawel.k1997@gmail.com>
2025-07-28 17:09:27 +0200

    Add a basic functions

---
+++ main.py
@@ -0,0 +1,15 @@
+def dodawanie(liczba1, liczba2):
+    return liczba1 + liczba2
+
+def odejmowanie(liczba1, liczba2):
+    return liczba1 - liczba2
+
+def mnozenie(liczba1, liczba2):
+    return liczba1 * liczba2
+
+def dzielenie(liczba1, liczba2):
+    return liczba1 / liczba2
+
+print("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie")
+dzialanie = int(input("Podaj działanie: "))
+
