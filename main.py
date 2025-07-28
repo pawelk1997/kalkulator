356e9744555da30024c6af8ea4ef83c37e464952
Paweł Kępczyński <pawel.k1997@gmail.com>
2025-07-28 17:13:25 +0200

    Add the ability to perform calculations

--- main.py
+++ main.py
@@ -13,3 +13,19 @@ def dzielenie(liczba1, liczba2):
 print("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie")
 dzialanie = int(input("Podaj działanie: "))
 
+liczba1 = int(input("Podaj pierwszą liczbę: "))
+liczba2 = int(input("Podaj drugą liczbę: "))
+
+if dzialanie == 1:
+    print(f"Wynik dodawania liczb {liczba1} oraz {liczba2} to {dodawanie(liczba1, liczba2)}")
+elif dzialanie == 2:
+    print(f"Wynik odejmowania liczb {liczba1} oraz {liczba2} to {odejmowanie(liczba1, liczba2)}")
+elif dzialanie == 3:
+    print(f"Wynik mnożenia liczb {liczba1} oraz {liczba2} to {mnozenie(liczba1, liczba2)}")
+elif dzialanie == 4:
+    if liczba1 == 0 or liczba2 == 0:
+        print("Dzielenie przez 0!")
+    else:
+        print(f"Wynik dzielenia liczb {liczba1} oraz {liczba2} to {dzielenie(liczba1, liczba2)}")
+else:
+    print("Podano nieprawidłowe działanie!")
